99bf96ffd56d0f9a221ef521325c0910cf6d4d46
chenlei <chenlei@likingfit.com>
2017-07-12 00:30:17 +0800

    first commit

---
+++ app/src/main/java/com/laboratory/chenlei/MainActivity.java
@@ -0,0 +1,18 @@
+package com.laboratory.chenlei;
+
+import android.media.browse.MediaBrowser;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.laboratory.chenlei.laboratoryapp.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        MediaBrowser
+    }
+}
---
+++ app/src/main/java/com/laboratory/chenlei/module/media/music/MusicProvider.java
@@ -0,0 +1,9 @@
+package com.laboratory.chenlei.module.media.music;
+
+/**
+ * Created by chenlei on 17/7/11.
+ */
+
+public class MusicProvider {
+
+}
