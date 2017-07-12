9b9fe6135f81a5f95922e66c19b71aaf6dff135c
chenlei <chenlei@likingfit.com>
2017-07-13 00:28:23 +0800

    music

--- app/src/main/java/com/laboratory/chenlei/MainActivity.java
+++ app/src/main/java/com/laboratory/chenlei/MainActivity.java
@@ -1,16 +1,42 @@
 package com.laboratory.chenlei;
 
-import android.media.browse.MediaBrowser;
+import android.content.ComponentName;
+import android.support.v4.media.MediaBrowserCompat;
+import android.support.v4.media.session.MediaSessionCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.laboratory.chenlei.module.media.music.MusicService;
 
 
 public class MainActivity extends AppCompatActivity {
 
+    private MediaBrowserCompat mediaBrowser ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mediaBrowser = new MediaBrowserCompat(this, new ComponentName(this, MusicService.class), connectionCallback, null);
     }
+
+    private MediaBrowserCompat.ConnectionCallback connectionCallback = new MediaBrowserCompat.ConnectionCallback() {
+
+        @Override
+        public void onConnected() {
+            super.onConnected();
+
+           MediaSessionCompat.Token token =  mediaBrowser.getSessionToken();
+        }
+
+        @Override
+        public void onConnectionSuspended() {
+            super.onConnectionSuspended();
+        }
+
+        @Override
+        public void onConnectionFailed() {
+            super.onConnectionFailed();
+        }
+    };
 }
